2334fb28ef07364551dcb6ddaa93f97ff2c58f50
Karl Zschiebsch <karl.zschiebsch@gmail.com>
2024-04-18 15:32:27 +0200

    Add venv for freeze

--- freeze.sh
+++ freeze.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Hello from the seekers community! cx_Freeze helper 24.4.9"
+echo "Hello from the seekers community! cx_Freeze helper 24.4.18"
 
 usage() {
   echo "Usage: $0 [-i|-s|-c|-t|-h]" 1>&2
@@ -8,21 +8,23 @@ usage() {
 
 install_requirements() {
   echo "Install requirements ..."
-  pip install cx_Freeze
+  python -m venv venv
+  venv/bin/pip install -r requirements.txt
+  venv/bin/pip install cx_Freeze
 }
 
 build_seekers() {
   echo "Building seekers ..."
-  cxfreeze -c run_seekers.py --target-dir dist --include-files config.ini
+  venv/bin/cxfreeze -c run_seekers.py --target-dir dist/seekers --include-files config.ini
 }
 
 build_client() {
   echo "Building client ..."
-  cxfreeze -c run_client.py --target-dir dist --include-files config.ini
+  venv/bin/cxfreeze -c run_client.py --target-dir dist/client --include-files config.ini
 }
 
-start() {
-  echo "Starting game ..."
+test() {
+  echo "Testing build ..."
   dist/run_seekers examples/ai-decide.py examples/ai-simple.py
 }
 
@@ -39,7 +41,7 @@ while getopts ":isct" arg; do
             build_client
             ;;
         t)
-            start
+            test
             ;;
         *)
             usage
