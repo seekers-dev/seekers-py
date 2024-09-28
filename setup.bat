2105b335542e47f4223a70be09dcc3e34b2e2be4
Kiyotoko <karl.zschiebsch@gmail.com>
2024-09-28 20:13:19 +0200

    Update submodules

--- setup.bat
+++ setup.bat
@@ -5,7 +5,7 @@ echo "Install requirements ..."
 .\venv\bin\pip install -r requirements.txt
 
 echo "Update submodule ..."
-git submodule update --init --recursive
+git submodule update --init --recursive --remote
 
 echo "Compile proto files"
 cd seekers-api
